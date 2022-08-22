785b211761092f166fb7875aebcd5cfb77adb29e
Brian Irons <the.brian.irons@gmail.com>
2022-08-22 11:29:04 +0200

    update to index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const SWAPI = require("./datasources/SWAPI");
 
-const server = new ApolloServer({
-  dataSources: () => ({
-    swApi: new SWAPI(),
-  }),
-  typeDefs,
-  resolvers,
-});
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+      return {
+        swApi: new SWAPI(),
+      };
+    },
+  });
 
-const { url, port } = await server.listen({port: process.env.PORT || 4000});
-console.log(`
+  const { url, port } = await server.listen({ port: process.env.PORT || 4000 });
+  console.log(`
     🚀  Server is running
     🔉  Listening on port ${port}
     📭  Query at ${url}
